9c3c52cdb03dbf9ba76431564624d2d316b54c01
Mille Codex <39792005+millecodex@users.noreply.github.com>
2023-01-20 14:17:04 +1300

    updated criticality to fill in gaps

--- criticality.py
+++ criticality.py
@@ -1,137 +1,400 @@
-import pandas as pd
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import os
+import sys
 import time
 import math
-
-# Read CSV file into DataFrame df
-df = pd.read_csv('drive/MyDrive/PhDstuffs/CMCdata_200.csv', index_col=0)
-
-# Show dataframe
-#print(df)
-
-df2 = pd.read_csv('drive/MyDrive/PhDstuffs/Project_Criticality_all.csv')
-
-# Show dataframe
-#df2.head()
-
-# add columns to CMC data for criticality
-#df['criticality']='none'
-#df.head()
-
-# subset dataframes for testing
-dfs = df.iloc[:10]
-df2s= df2.iloc[:50]
-
-# searching by project name to pull criticality
-"""This results in many named duplicates, for example:
-Found: Terra, 0.44098000000000004
-Found: Terra, 0.11259000000000001
-Found: Terra, 0.11035
-Found: Monero, 0.6229899999999999
-Found: Cosmos, 0.63302
-Found: Cosmos, 0.5687
-Found: Cosmos, 0.4926
-Found: Cosmos, 0.43818
-Found: Cosmos, 0.38327
-Found: Cosmos, 0.36705
-Found: Cosmos, 0.2116
-Found: Solana, 0.67805
-Found: IOTA, 0.25358
-Found: IOTA, 0.16330999999999998
-"""
-for row in df.itertuples():
-  name = row.name
-  for row2 in df2.itertuples():
-    if name.casefold() == str(row2.name).casefold():
-      criticality=row2.criticality_score
-      print("Found: "+name+", "+str(criticality))
-
-# search by url ensures unique results, however many are missed
-# manual dataset updating required for the url field
-start = time.process_time()
-# about 41s to process top cmc top 100 via COLAB
-num=1
-for row in df.itertuples():
-  # only search for strings; floats (NaN) are skipped
-  if isinstance(row.Source_code, str):
-    url = str(row.Source_code)
-    # trim possible '/' from CMCdata source_code strings (should do this when parsing the cmc data)
-    if url[-1]=='/':
-      url=url[:-1]
-    # loop through df2 (criticality) looking for source code url
-    for row2 in df2.itertuples():
-      if url == row2.url:
-        criticality=row2.criticality_score
-        print(str(num)+" "+url+",\t"+str(criticality))
-        num+=1
-        break
-end = time.process_time()
-print("time elapsed = ", end - start)
-
-# 49 in cmc top 100 found:
-"""
-1 https://github.com/bitcoin/bitcoin,	      0.86864
-2 https://github.com/ethereum/go-ethereum,	0.8229700000000001
-3 https://github.com/input-output-hk/cardano-node,	0.6350399999999999
-4 https://github.com/paritytech/polkadot,	  0.6197199999999999
-5 https://github.com/ripple/rippled,	      0.63159
-6 https://github.com/Uniswap/uniswap-v2-core,	0.19942000000000001
-7 https://github.com/smartcontractkit/chainlink,	0.6142
-8 https://github.com/stellar/stellar-core,	0.59529
-9 https://github.com/dogecoin/dogecoin,	    0.51976
-10 https://github.com/aave/aave-protocol,	  0.25848000000000004
-11 https://github.com/vechain/thor,	        0.40387
-12 https://github.com/monero-project/monero,	0.6229899999999999
-13 https://github.com/cosmos/cosmos-sdk,	  0.7429399999999999
-14 https://github.com/solana-labs/solana,	  0.67805
-15 https://github.com/iotaledger/iota.js,	  0.41771
-16 https://github.com/EOSIO/eos,	          0.7085100000000001
-17 https://github.com/bitcoin-sv/bitcoin-sv,	0.47458999999999996
-18 https://github.com/tronprotocol/java-tron,	0.62134
-19 https://github.com/ava-labs/avalanchego,	0.50151
-20 https://github.com/sushiswap/sushiswap,	0.30666
-21 https://github.com/neo-project/neo,	    0.6212
-22 https://github.com/filecoin-project/lotus,	0.66433
-23 https://github.com/algorand/go-algorand,	0.5318
-24 https://github.com/makerdao/dss,	        0.45585
-25 https://github.com/ElrondNetwork/elrond-go,	0.56316
-26 https://github.com/Synthetixio/synthetix,	0.57295
-27 https://github.com/graphprotocol/graph-node,	0.55579
-28 https://github.com/decred/dcrd,	        0.59037
-29 https://github.com/makerdao/market-maker-keeper,	0.32309
-30 https://github.com/compound-finance/compound-protocol,	0.33549
-31 https://github.com/Zilliqa/Zilliqa,	    0.59758
-32 https://github.com/zcash/zcash,	        0.75193
-33 https://github.com/RavenProject/Ravencoin,	0.55743
-34 https://github.com/bancorprotocol/contracts-solidity,	0.5311
-35 https://github.com/wavesplatform/Waves,	0.61635
-36 https://github.com/0xProject/0x-monorepo,	0.5889800000000001
-37 https://github.com/blockstack/stacks-blockchain,	0.65277
-38 https://github.com/ontio/ontology,	      0.44072
-39 https://github.com/iost-official/go-iost,	0.4885
-40 https://github.com/nanocurrency/nano-node,	0.54171
-41 https://github.com/qtumproject/qtum,	    0.5528
-42 https://github.com/harmony-one/harmony,	0.5965
-43 https://github.com/BTCGPU/BTCGPU,	      0.42257
-44 https://github.com/golemfactory/golem,	  0.43501999999999996
-45 https://github.com/ArweaveTeam/arweave,	0.43885
-46 https://github.com/OrchidTechnologies/orchid,	0.47928000000000004
-47 https://github.com/storj/storj,	        0.56045
-48 https://github.com/vechain/thor,	        0.40387
-49 https://github.com/steemit/steem,	      0.47662
-"""
+import json
+import pandas as pd
 
 
-# search by using pandas column search methods
-# function from stackexchange
-# see: https://stackoverflow.com/questions/26640129/search-for-string-in-all-pandas-dataframe-columns-and-filter
+# In[ ]:
+
+
+# #--------------------------------------------------------------
+# #-- S E A R C H   C R I T I C A L I T Y   S P R E A D S H E E T
+# #--------------------------------------------------------------
+# # using the csv file of CRITICALITY_SCORE and searching for 
+# # repos from 200_repos; this excludes new/current projects
+# # download: https://storage.cloud.google.com/ossf-criticality-score
+# # UDPATE: looks like a monthly run is published here (wish I had of known!)
+# #--------------------------------------------------------------
+# # Read '200_repos.csv' into DataFrame df
+# #
+# # NaN is assigned to empty cells
+# df = pd.read_csv('200_repos.csv')
+# dfs = df[['CMC_id', 'source_code', 'forge']].copy()
+# dfc = pd.read_csv('project_criticality_all.csv')
+# num = 0
+# for row in dfs.itertuples():
+#     # only search if github
+#     if row.forge == 'github':
+#         # only search for strings; floats (NaN) are skipped
+#         if isinstance(row.source_code, str):
+#             url = str(row.source_code)
+#             # loop through df2 (criticality) looking for source code url
+#             for row2 in dfc.itertuples():
+#                 if url == row2.url:
+#                     dfs.at[row.Index, 'criticality'] = row2.criticality_score
+#                     num += 1
+#                     break
+#             sys.stdout.write(".")
+#             sys.stdout.flush()
+# print(str(num), 'criticality scores found and updated')
+
+# # update MERGED sheet with new data
+# # 'CMC_id' is the key, drop 'repo', and 'forge' before the merge
+# # to prevent duplicate columns
+# dfs.drop(columns = ['source_code', 'forge'], inplace = True)
+# dfm = pd.merge(df,dfs,on = ['CMC_id'], how = 'outer')
+
+# # write out new data
+# dfs.to_csv('200_crit.csv', encoding='utf-8', index = 0)
+# dfm.to_csv('200_merged.csv', encoding='utf-8', index = 0)
+
+
+# In[ ]:
+
+
+#-------------------------------------------------
+# -- C A L L   C R I T I C A L I T Y _ S C O R E--
+# require github token and command line access----
+#-------------------------------------------------
 #
-def search(regex: str, df, case=False):
-    """Search all the text columns of `df`, return rows with any matches."""
-    textlikes = df.select_dtypes(include=[object, "string"])
-    return df[
-        textlikes.apply(
-            lambda column: column.str.contains(regex, regex=True, case=case, na=False)
-        ).any(axis=1)
-    ]
-out=search("https://github.com/rust-lang/rust",df2)
+# Input:
+# 'cmc_repos_forge.csv' from prepare_repos.ipynb
+#
+# Outputs:
+# 'crit_output_all.csv' contains all the info; see sample output below
+# 'crit_only.csv' contains two columns: 'url:','criticality_score:'
+#
+# >> repo: https://github.com/ossf/criticality_score
+# 0. make sure github access token is exported to PATH (see methodology notes)
+# 1. install: pip3 install criticality-score
+# 2. check PATH: WARNING: The script criticality_score is installed in '/home/user/.local/bin' which is not on PATH.
+# >> export PATH="/home/user/.local/bin:$PATH"
+# 3. get 'GITHUB_AUTH_TOKEN' and export path on command line or set env variable in jupyter
+# 
+# Set the environment variable 'GITHUB_AUTH_TOKEN' from Jupyter
+# (this is a short-cut; in the future look into pycrosskit)
+# >key = 'GITHUB_AUTH_TOKEN'
+# >os.environ[key] = 'secret'
+#
+# read out the value
+# >value = os.getenv(key)
+# >print("Value of 'GITHUB_AUTH_TOKEN' environment variable :", value) 
+#
+# 4. run: criticality_score --repo https://github.com/bitcoin/bitcoin
+# >sample output:
+# '''     
+# ['name: bitcoin',
+#  'url: https://github.com/bitcoin/bitcoin',
+#  'language: C++',
+#  'created_since: 142',
+#  'updated_since: 0',
+#  'contributor_count: 961',
+#  'org_count: 4',
+#  'commit_frequency: 54.8',
+#  'recent_releases_count: 3',
+#  'updated_issues_count: 1920',
+#  'closed_issues_count: 1467',
+#  'comment_frequency: 2.7',
+#  'dependents_count: 348588',
+#  'criticality_score: 0.86651']
+# '''
+
+
+# In[2]:
+
+
+key = 'GITHUB_AUTH_TOKEN'
+os.environ[key] = 'xxx'
+
+
+# In[3]:
+
+
+value = os.getenv(key)
+print("Value of 'GITHUB_AUTH_TOKEN' environment variable :", value) 
+
+
+# In[37]:
+
+
+# Read '200_repos.csv' into DataFrame df
+#df = pd.read_csv('cmc_repos_forge.csv')
+df = pd.read_csv('merged_Jan_2023.csv')
+# keep 'source_code' location and 'forge'
+df_in = df[['source_code', 'forge', 'language:']].copy()
+df_in = df_in.rename(columns={'language:': 'language'})
+# subset dataframes for testing
+df_test1 = df_in.iloc[99:104].copy()
+df_test2 = df_in.iloc[:20].copy()
+df_test3 = df_in.iloc[99:104].copy()
+
+
+# In[39]:
+
+
+df200 = df_in.iloc[:199].copy()
+df400 = df_in.iloc[200:399].copy()
+df600 = df_in.iloc[400:].copy()
+
+
+# In[7]:
+
+
+# ------------------------------------------------
+# dfParse builds a dataFrame using bash output
+#  @output the command line output from calling criticality_score
+#  @firstTime boolean to initialize the dataframe the first loop call
+#  @dataframe the dataframe to be updated and returned
+# ------------------------------------------------
+def dfParse(output, firstUpdate, dataframe):
+    jout = json.dumps(output)    #jout is a str
+    out_dict = json.loads(jout)   #out_dict is a list
+    
+    # catch a possible traceback
+    if 'Traceback' in out_dict[0]:
+        print('found traceback')
+        return dataframe
+    
+    # prepare the dataFrame, initialize with column headers the same
+    # as the criticality_score output
+    df = pd.DataFrame(out_dict)
+    df.rename(columns = {0:'metric'}, inplace = True)
+    df[['metric','value']] = df.metric.str.split(expand = True)
+    df = df.transpose(copy = True)
+
+    # remove index column (with labels 'metric' & 'value')
+    # and reset the index
+    df.reset_index(drop=True, inplace=True)
+
+    # rename columns according to first row; then drop the row
+    df = df.rename(columns = df.iloc[0]).drop(df.index[0])
+
+    if firstUpdate:
+        dataframe = df.copy()
+    else:
+        # append row[1] to df
+        dataframe = dataframe.append(df, ignore_index = True)   
+        
+    return dataframe
+
+
+# In[ ]:
+
+
+# ------------------------------------------------------
+# main loop requires dataFrame: 'df_in'
+#                      returns: 'df_out'
+# df_out does not have CMC_id and some will be missed;
+# should be able to merge back on 'url:'=='source_code'
+# ------------------------------------------------------
+# This takes a while, criticality_score has a built-in
+# rate limiter for handling github API limits. It still 
+# gets stuck over about 200 url requests.
+# for example:
+# >Rate limit exceeded, sleeping till reset: 334 seconds.
+# ------------------------------------------------------
+# Sample Output:
+#  223 total projects evaluated
+#  153 criticality scores updated
+#  70 repos private or missing
+#  Total time elapsed: 22.6 minutes
+# ------------------------------------------------------
+
+# Need to chunk this better to avoid the rate limiting and
+# its confusing with 200/400/600, etc.
+#df_in = df200.copy()
+#df_in = df400.copy()
+df_in = df600.copy()
+# df_in = df_test3.copy()
+
+start = time.time()
+total = 0
+updated = 0
+firstUpdate = True
+df_out = pd.DataFrame
+
+for row in df_in.itertuples():
+    # proceed if github and source_code is a string and not private
+    # added check 'and (pd.isna(row.language))' to update criticality columns Jan.2023, remove for future use
+    if (row.forge == 'github') and isinstance(row.source_code, str) and (row.source_code != 'private') and (pd.isna(row.language)):
+        cmd = 'criticality_score --repo ' + row.source_code
+        print(row.source_code)
+        output = get_ipython().getoutput('{cmd}')
+        # if first element is ['name':'bitcoin'], output is as expected, can parse
+        if 'name' in output[0]: 
+            df_out = dfParse(output, firstUpdate, df_out)
+            firstUpdate = False
+            updated += 1
+    total += 1
+    sys.stdout.write(".")
+    sys.stdout.flush()
+    
+# log some output with a timer
+print('\n',str(df_in.shape[0]), 'total projects evaluated\n', 
+      str(updated), 'criticality scores updated\n', 
+      str(total - updated), 'repos private or missing\n', 
+      'Total time elapsed:', round((time.time() - start)/60, 1), 'minutes')
+
+#df200 = df_out.copy()
+#df400 = df_out.copy()
+df600 = df_out.copy()
+
+
+# In[50]:
+
+
+# write out new data
+df200.to_csv('crit_output_200_Jan2023.csv', encoding='utf-8', index = 0)
+df400.to_csv('crit_output_400_Jan2023.csv', encoding='utf-8', index = 0)
+df600.to_csv('crit_output_600_Jan2023.csv', encoding='utf-8', index = 0)
+# dft = df_out[['url:','criticality_score:']].copy()
+# dft.to_csv('crit_only.csv', encoding='utf-8', index = 0)
+
+
+# In[ ]:
+
+
+# read in chunked data
+# df200 = pd.read_csv('crit_output_all_0-200.csv')
+# df400 = pd.read_csv('crit_output_all_200-400.csv')
+# df600 = pd.read_csv('crit_output_all_400-600.csv')
+# dfcrit200 = pd.read_csv('crit_only_0-200.csv')
+# dfcrit400 = pd.read_csv('crit_only_200-400.csv')
+# dfcrit600 = pd.read_csv('crit_only_400-600.csv')
+df200 = pd.read_csv('crit_output_all_200.csv')
+
+
+# In[46]:
+
+
+# merge data frames
+dfall = pd.concat([df200, df400, df600], ignore_index=True)
+# dfcrit = pd.concat([dfcrit200, dfcrit400, dfcrit600], ignore_index=True)
+
+
+# In[48]:
+
+
+# write out merged frames
+dfall.to_csv('crit_output_Jan2023.csv', encoding='utf-8', index = 0)
+# dfcrit.to_csv('crit_only.csv', encoding='utf-8', index = 0)
+
+
+# In[89]:
+
+
+# merge criticality with cmc_repos_forge
+#cmc_repos_forge = pd.read_csv(r"/home/japple/localDev/health/cmc/cmc_repos_forge.csv")
+
+original = pd.read_csv('merged_Jan_2023.csv')
+
+
+# In[ ]:
+
+
+# [crit_output_all] 'url:' == [cmc_repos_forge] 'source_code'
+dfall.drop(columns = ['name:'], inplace = True)
+
+
+# In[ ]:
+
+
+# rename [crit_output_all] 'url:' to 'source_code' to match
+dfall.rename(columns = {'url:':'source_code'}, inplace = True)
+
+
+# In[49]:
+
+
+dfm = pd.merge(original, dfall, on = ['source_code'], how = 'outer')
+#dfm.sort_values(by='CMC_rank', ascending=False)
+#dfm.to_csv('merged_Jan2023.csv', encoding='utf-8', index=True)
+
+
+# In[90]:
+
+
+# loop to update criticality gaps
+original['source_code'] = original['source_code'].str.lower()
+dfall['source_code'] = dfall['source_code'].str.lower()
+for i, row in original.iterrows():
+    if row['source_code'] in dfall['source_code'].values:
+        dfall_row = dfall.loc[dfall['source_code'] == row['source_code']]
+        original.loc[i, 'language:'] = dfall_row['language:'].values[0]
+        original.loc[i, 'created_since:'] = dfall_row['created_since:'].values[0]
+        original.loc[i, 'updated_since:'] = dfall_row['updated_since:'].values[0]
+        original.loc[i, 'contributor_count:'] = dfall_row['contributor_count:'].values[0]
+        original.loc[i, 'org_count:'] = dfall_row['org_count:'].values[0]
+        original.loc[i, 'commit_frequency:'] = dfall_row['commit_frequency:'].values[0]
+        original.loc[i, 'recent_releases_count:'] = dfall_row['recent_releases_count:'].values[0]
+        original.loc[i, 'updated_issues_count:'] = dfall_row['updated_issues_count:'].values[0]
+        original.loc[i, 'closed_issues_count:'] = dfall_row['closed_issues_count:'].values[0]
+        original.loc[i, 'comment_frequency:'] = dfall_row['comment_frequency:'].values[0]
+        original.loc[i, 'dependents_count:'] = dfall_row['dependents_count:'].values[0]
+        original.loc[i, 'criticality_score:'] = dfall_row['criticality_score:'].values[0]
+
+
+# In[92]:
+
+
+original.to_csv('crit_updated_Jan2023.csv', encoding='utf-8', index=True)
+
+
+# In[ ]:
+
+
+# merge alexa with merged_checked_manually
+dfalexa = pd.read_csv(r"/home/japple/localDev/health/webRank/alexa.csv")
+dfsource = pd.read_csv("merged_checked_manually.csv")
+# merge on CMC_id
+dft=dfalexa[['CMC_id', 'alexa_rank']].copy()
+
+
+# In[ ]:
+
+
+dfm=pd.merge(dfsource,dft,on=['CMC_id'], how='outer')
+dfm.sort_values(by='CMC_rank', ascending=False)
+
+
+# In[96]:
+
+
+# ------------------------------------------------------------
+# --C H E C K   F O R   D U P L I C A T E S-------------------
+# CMC_id column *may* contain duplicate entries (CAKE, FUN!?)
+# ------------------------------------------------------------
+if dfm[dfm.duplicated(['CMC_id'], keep='last')].empty:
+    print('No duplicate CMC_ids were found')
+else: 
+    num = len(dfm[dfm.duplicated(['CMC_id'], keep='last')])
+    print(num, 'duplicates were found and deleted based on CMC_id:')
+    print(dfm[dfm.duplicated(['CMC_id'], keep='last')])
+    
+    # delete duplicates keeping 2nd entry
+    dfm.drop_duplicates(subset=['CMC_id'], keep='last', inplace=True)
+
+
+# In[95]:
+
+
+dfm.to_csv('merged.csv', encoding='utf-8', index=False)
+
+
+# In[ ]:
+
+
+# # store a dataframe in the kernel
+# %store dfm
+
+# # retrieve a datafram
+# %store -r dfm
 
